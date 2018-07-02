3d0cf1d489122c152abe64d761c833e83b6accaa
Emanuele Malfara <emanuele.malfara@trovaprezzi.it>
2018-07-02 14:45:58 +0200

    prima versione, pochi test verdi.

--- exercise.rb
+++ exercise.rb
@@ -1,4 +1,18 @@
 class Exercise
   def edit_distance(from, to)
+    from_split = from.split("")
+    to_split = to.split("")
+    changes = 0
+    0.upto(to_split.size - 1) do |index|
+      puts "confronto #{to_split[index]} con #{from_split[index]} in posizione #{index}"
+      if to_split[index] != from_split[index]
+        changes += 1
+      end
+    end
+    (from_split.size-1).downto(to_split.size) do |index|
+      puts "rimuovo carattere #{from_split[index]} in posizione #{index}"
+        changes += 1
+    end
+    changes
   end
 end
--- test_exercise.rb
+++ test_exercise.rb
@@ -2,7 +2,24 @@ require_relative "exercise"
 require "test/unit"
 
 class TestExercise < Test::Unit::TestCase
-  test 'exercise test 0  ' do
+  test 'test exercise' do
     assert_equal(3, Exercise.new().edit_distance('kitten', 'sitting'))
+  end
+
+  test 'add' do
+    assert_equal(2, Exercise.new().edit_distance('0123', '012345'))
+  end
+
+  test 'remove' do
+    assert_equal(2, Exercise.new().edit_distance('012345', '0123'))
+  end
+
+
+  test 'add and remove' do
+    assert_equal(2, Exercise.new().edit_distance('01234', '12345'))
+  end
+
+  test 'add remove and change' do
+    assert_equal(3, Exercise.new().edit_distance('012345678', '123446789'))
   end
 end
