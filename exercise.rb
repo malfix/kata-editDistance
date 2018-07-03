1095ebb5fefc51d8812ec4ed2b8224fb68ee8c40
Emanuele Malfara <emanuele.malfara@trovaprezzi.it>
2018-07-03 08:49:38 +0200

    * tutti i test verdi

--- exercise.rb
+++ exercise.rb
@@ -2,17 +2,24 @@ class Exercise
   def edit_distance(from, to)
     from_split = from.split("")
     to_split = to.split("")
-    changes = 0
-    0.upto(to_split.size - 1) do |index|
-      puts "confronto #{to_split[index]} con #{from_split[index]} in posizione #{index}"
-      if to_split[index] != from_split[index]
-        changes += 1
+    [get_best(from_split, to_split), get_best(to_split, from_split)].min
+  end
+
+  def get_best(first_word, second_word)
+    best_changes_count = [first_word.size, second_word.size].max
+    0.upto(first_word.size-1) do |start|
+      current_changes_count = start
+      0.upto(second_word.size-1) do |index_char|
+          if first_word[start+index_char] && second_word[index_char] != first_word[start+index_char]
+            current_changes_count += 1
+          end
+      end
+      chars_to_remove =  start + (second_word.size - first_word.size).abs
+      current_changes_count += chars_to_remove > 0 ? chars_to_remove : 0
+      if current_changes_count < best_changes_count
+        best_changes_count = current_changes_count
       end
     end
-    (from_split.size-1).downto(to_split.size) do |index|
-      puts "rimuovo carattere #{from_split[index]} in posizione #{index}"
-        changes += 1
-    end
-    changes
+    best_changes_count
   end
 end
